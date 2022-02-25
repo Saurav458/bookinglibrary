ceb5eb686d98cb4b006076f9ca222eef34f69a20
saurav <jainsaurav0503@gmail.com>
2022-02-25 13:05:31 +0530

    adding app.js file

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import Login from "./Components/Login/Login";
 import Register from "./Components/Register/Register";
 import Header from "./Components/Header/Header";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Addbook from "./Components/AddBook/Addbook";
+// import Addbook from "./Components/AddBook/Addbook";
+// import Pop from "./Components/Popupp/Pop";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Router>
+          <Router>
         <Switch>
           <Route exact path="/">
           <Login />
@@ -21,13 +22,11 @@ class App extends Component {
             <Register/>
           </Route>
         </Switch>
-      </Router>
-        {/* <Login/> */}
-         {/* <Register/>  */}
-        {/* <Header/> */}
-        {/* <Homepage/> */}
-        {/* <Links/> */}
+      </Router>  
+     
+     
         {/* <Addbook/> */}
+        {/* <Pop/> */}
       </div>
     );
   }
